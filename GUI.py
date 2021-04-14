0525e3125bde5b6d9ed6d71bda3e1e8fcc70c3df
HyperTitian <handsomejack414307@mgmail.com>
2021-04-14 15:16:38 -0400

    I'm very proud of the orange button

--- GUI.py
+++ GUI.py
@@ -6,12 +6,23 @@ This is a temporary script file.
 """
 
 import tkinter as tkr
+from tkinter import Button
+from tkinter import PhotoImage
 
 tk = tkr.Tk()
-canvas = tkr.Canvas(tk, width=500, height=500)
+
+canvas = tkr.Canvas(tk, width = 250, height = 250)
 canvas.grid()
+
+button = Button(tk, text = "I'm a button")
+button.place(x = 100, y = 100)
+
+button1 = Button(tk, text = "I'm another button")
+button1.place(x = 100, y = 150)
+
+orangeButton = Button(tk, highlightthickness = 0, bd = 0)
+png = tkr.PhotoImage(file = "C:/Users/hands/MIS2100 Group Project/OrangeButton.png", master = tk)
+orangeButton.config(image = png)
+orangeButton.place(x = 100, y = 200)
+
 tk.mainloop()
-
-"I just want to see if this updates"
-
-"This should absolutely prove that it works"
