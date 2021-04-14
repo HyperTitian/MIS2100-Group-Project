1564c578536a11f5c1e92b1884e5930593eb05fd
HyperTitian <handsomejack414307@mgmail.com>
2021-04-14 13:02:25 -0400

    first commit

---
+++ temp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import tkinter as tkr
+
+tk = tkr.Tk()
+canvas = tkr.Canvas(tk, width=500, height=500)
+canvas.grid()
+tk.mainloop()
