15e6b2724c3c4aee7a2d2b0684a2f238c5ccc033
HyperTitian <handsomejack414307@mgmail.com>
2021-04-20 13:00:29 -0400

    Everything besides the input text fields

--- GUI.py
+++ GUI.py
@@ -5,24 +5,57 @@ Spyder Editor
 This is a temporary script file.
 """
 
-import tkinter as tkr
-from tkinter import Button
-from tkinter import PhotoImage
+# The important stuff
+from tkinter import *
+root = Tk()
+root.geometry("600x600")
 
-tk = tkr.Tk()
+# Import pictures
+bg = PhotoImage(file = "C:/Users/hands/MIS2100 Group Project/Background.png", master = root)
+string = PhotoImage(file = "C:/Users/hands/MIS2100 Group Project/String.png", master = root)
+text = PhotoImage(file = "C:/Users/hands/MIS2100 Group Project/Text.png", master = root)
+button = PhotoImage(file = "C:/Users/hands/MIS2100 Group Project/Button.png", master = root)
 
-canvas = tkr.Canvas(tk, width = 250, height = 250)
-canvas.grid()
+# Frame the GUI
+canvas = Canvas(root, width = 400, height = 400, highlightthickness = 0)
+canvas.pack(fill = "both", expand = True)
+canvas.create_image(0, 0, image = bg, anchor = "nw")
 
-button = Button(tk, text = "I'm a button")
-button.place(x = 100, y = 100)
+string1 = Label(root, width = 200, height = 20, image = string, highlightthickness = 2)
+string1.place(x = 50, y = 70)
 
-button1 = Button(tk, text = "I'm another button")
-button1.place(x = 100, y = 150)
+string2 = Label(root, width = 200, height = 20, image = string, highlightthickness = 2)
+string2.place(x = 50, y = 130)
 
-orangeButton = Button(tk, highlightthickness = 0, bd = 0)
-png = tkr.PhotoImage(file = "C:/Users/hands/MIS2100 Group Project/OrangeButton.png", master = tk)
-orangeButton.config(image = png)
-orangeButton.place(x = 100, y = 200)
+string3 = Label(root, width = 200, height = 20, image = string, highlightthickness = 2)
+string3.place(x = 340, y = 70)
 
-tk.mainloop()
+string4 = Label(root, width = 200, height = 20, image = string, highlightthickness = 2)
+string4.place(x = 340, y = 130)
+
+string5 = Label(root, width = 200, height = 20, image = string, highlightthickness = 2)
+string5.place(x = 340, y = 190)
+
+text1 = Label(root, width = 200, height = 200, image = text, highlightthickness = 2)
+text1.place(x = 50, y = 240)
+
+text2 = Label(root, width = 200, height = 200, image = text, highlightthickness = 2)
+text2.place(x = 340, y = 240)
+
+# Things that move
+button1 = Button(root, width = 50, height = 50, image = button)
+button1.place(x = 50, y = 460)
+
+button2 = Button(root, width = 50, height = 50, image = button)
+button2.place(x = 340, y = 460)
+
+output1 = Text(root, width = 19, height = 10, bg = "#071A33", fg = "white", wrap = WORD, borderwidth = 0)
+output1.place(x = 72, y = 276)
+output1.insert(END, "This is output field 1")
+
+output2 = Text(root, width = 19, height = 10, bg = "#071A33", fg = "white", wrap = WORD, borderwidth = 0)
+output2.place(x = 362, y = 276)
+output2.insert(END, "This is output field 2")
+
+# The really important stuff
+root.mainloop()
